475912e446286b93275a37a8a3db2010492ab0ae
Abhimanyu Singh <122745699+Abhi-boostin@users.noreply.github.com>
2025-02-06 18:49:32 +0530

    Configure GitHub Pages deployment

--- next.config.js
+++ next.config.js
@@ -1,8 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   images: {
+    unoptimized: true,
     domains: ['img.youtube.com', 'lastfm.freetls.fastly.net'],
   },
+  basePath: process.env.NODE_ENV === 'production' ? '/<your-repo-name>' : '',
 }
 
 module.exports = nextConfig 
